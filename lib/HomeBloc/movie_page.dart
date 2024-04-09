9654228fbc28adff8697f4a6b59ef0c7fc24bb37
Christian Rivero <christianriveroarnez@gmail.com>
2024-04-09 08:36:01 -0400

    added back header to payment page

--- lib/HomeBloc/movie_page.dart
+++ lib/HomeBloc/movie_page.dart
@@ -69,7 +69,7 @@ class _HomePageState extends State<HomePage> {
                   ),
                   ElevatedButton(
                       onPressed: () {
-                        Navigator.pushReplacement(
+                        Navigator.push(
                             context,
                             MaterialPageRoute(
                                 builder: (context) =>
